d5c17596143374698d738b750f64c9e87ee5ba3f
Andrew Blackwell <andrew@balletmecanique.eu>
2017-10-20 10:49:34 +0200

    rounder profile picture radius

--- resources/views/memberprofile.blade.php
+++ resources/views/memberprofile.blade.php
@@ -2,7 +2,7 @@
   <div class="container alert alert-secondary" style="max-width: 100%; background-color: #efefef; margin-bottom: 0px;">
       <div class="row">
         <div class="col-sm-4 col-5">
-          <img src="https://ctc-members.dk/media/{{$portrait}}" alt="" style="max-width: 100%; border: lightgrey solid 2px; border-radius: 7%;">
+          <img src="https://ctc-members.dk/media/{{$portrait}}" alt="" style="max-width: 100%; border: lightgrey solid 2px; border-radius: 8%;">
         </div>
         <div class="col-sm-8 col-7">
           <h1 class="display-4">{{$first_name}}</h1><h1 class="display-4">{{$last_name}}</h1>
