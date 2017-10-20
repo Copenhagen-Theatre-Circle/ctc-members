53b30a6ece7cb7a06357335ecc0176291fd11b0d
Andrew Blackwell <andrew@balletmecanique.eu>
2017-10-20 11:38:55 +0200

    completed v1 of memberprofile view

--- resources/views/memberprofile.blade.php
+++ resources/views/memberprofile.blade.php
@@ -1,5 +1,5 @@
 
-  <div class="container alert alert-secondary" style="max-width: 100%; background-color: #efefef; margin-bottom: 0px;">
+  <div class="container alert alert-secondary" style="max-width: 100%; background-color: #efefef; margin-bottom: 0px; padding: 30px;">
       <div class="row">
         <div class="col-sm-4 col-5">
           <img src="https://ctc-members.dk/media/{{$portrait}}" alt="" style="max-width: 100%; border: lightgrey solid 2px; border-radius: 8%;">
@@ -9,13 +9,74 @@
         </div>
       </div>
       <div class="row">
-        <div class="col">
+        <div class="col-xs-12">
           <br/>
           <blockquote class="blockquote" style="font-size: 16px;"><p>
-            {!! nl2br(e($biography)) !!}
+            "{!! nl2br(e($biography)) !!}"
           </p></blockquote>
         </div>
       </div>
+
+      @if ($general_interests)
+
+        <div class="row">
+          <div class="col-xs-12" style="line-height: 36px;">
+
+                <h3>General Interests:</h3>
+
+                @foreach ($general_interests as $general_interest)
+
+                  <span class='badge badge-primary' style='font-size: 18px; background-color:#{{$general_interest['color_hex']}}'>{{$general_interest['name']}}</span> &nbsp;
+
+                @endforeach
+
+              <br/>
+
+          </div>
+        </div>
+
+      @endif
+
+      @if ($experience)
+
+        <div class="row">
+          <div class="col-xs-12" style="line-height: 180%;">
+
+                <h3>Experience:</h3>
+
+                @foreach ($experience as $crewfunction)
+
+                  <span class='badge badge-pill badge-primary' style='font-size: 15px; background-color:#{{$crewfunction['color_hex']}}'>{{$crewfunction['name']}}</span> &nbsp;
+
+                @endforeach
+
+              <br/>
+
+          </div>
+        </div>
+
+      @endif
+
+      @if ($wants_to_learn)
+
+        <div class="row">
+          <div class="col-xs-12" style="line-height: 180%;">
+
+                <h3>Wants to Learn:</h3>
+
+                @foreach ($wants_to_learn as $crewfunction)
+
+                  <span class='badge badge-pill badge-light' style='font-size: 15px;background-color:#dddddd;color:black;'>{{$crewfunction['name']}}</span> &nbsp;
+
+                @endforeach
+
+              <br/>
+
+          </div>
+        </div>
+
+      @endif
+
 
   </div>
 
