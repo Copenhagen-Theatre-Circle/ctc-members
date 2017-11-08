e3169afbc39155cfa0786271d6ea1ab441a84804
Andrew Blackwell <andrew@balletmecanique.eu>
2017-11-08 11:32:10 +0100

    membership star only shown for members

--- resources/views/membership.blade.php
+++ resources/views/membership.blade.php
@@ -104,7 +104,9 @@
                             {{-- Membership Star --}}
 
                             <td class="hidden-xs-down" style="vertical-align:middle; text-align: center; width: 50px;">
-                                <img src="https://ctc-members.dk/media/star.png" alt="" style="object-fit: cover; height: 30px; width: 30px;  ">
+                              @if ($person->ismember())
+                                  <img src="https://ctc-members.dk/media/star.png" alt="" style="object-fit: cover; height: 30px; width: 30px;  ">
+                              @endif
                             </td>
 
                             {{-- Last Update --}}
