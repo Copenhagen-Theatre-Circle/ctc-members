d33b82a71a7c76830879d3a0a4df5910365a6bd7
Andrew Blackwell <andrew@balletmecanique.eu>
2017-11-20 08:56:41 +0100

    refactored membership controller

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -26,25 +26,48 @@ class MembersController extends Controller
   public function index()
   {
 
+    // query people with questionnaire_answers
+
+    $people = Person::with('questionnaire_answers');
+
+    // query if name search
+
     if (!empty(request('name'))) {
-      $people = Person::with('questionnaire_answers')->where(DB::raw('CONCAT(first_name, " ", last_name)'), 'like', '%' . request('name') . '%')->get();
-    } elseif (!empty(request('g'))) {
-      $people = Person::with('questionnaire_answers')->whereHas('questionnaire_answers', function($q){
-        $q->where('functiongroup_id',request('g'));
-      })->orderBy('first_name', 'asc')->orderBy('last_name', 'asc')->get();
-    } elseif (!empty(request('f'))) {
-      $people = Person::with('questionnaire_answers')->whereHas('questionnaire_answers', function($q){
-        $q->where(['interest'=>1,'function_id'=>request('f')]);
-      })->orderBy('first_name', 'asc')->orderBy('last_name', 'asc')->get();
-    }
-    else
-    {
-      $people=Person::orderBy('first_name', 'asc')->orderBy('last_name', 'asc')->get();
+      $people->where(DB::raw('CONCAT(first_name, " ", last_name)'), 'like', '%' . request('name') . '%');
     }
 
-    $people = $people->filter(function ($person) {
-    return $person->ismember();
+    // query if general interest search
+
+    if (!empty(request('g'))) {
+      $people->whereHas('questionnaire_answers', function($q){
+        $q->where('functiongroup_id',request('g'));
+      });
+    }
+
+    // query if specific interest search
+
+    if (!empty(request('f'))) {
+      $people->whereHas('questionnaire_answers', function($q){
+        $q->where(['interest'=>1,'function_id'=>request('f')]);
+      });
+    }
+
+    // show only members
+
+    $people->whereHas('memberships', function($q){
+      $minSeasonMembership = 48;
+      $q->where('season_id', '>', $minSeasonMembership);
     });
+
+    // order by
+
+    $people->orderBy('first_name', 'asc')->orderBy('last_name', 'asc');
+
+    // execute query
+
+    $people = $people->get();
+
+    // retrieve functiongroups and functions
 
     $functiongroups = Functiongroup::orderBy('sort_order')->get();
     $functions = Crewfunction::get()->sortBy('sort_order')->sortBy('FunctionGroupSortOrder');
@@ -53,8 +76,11 @@ class MembersController extends Controller
       $functionarray[$function->functiongroup][$function->id]=$function->questionnaire_name;
     }
 
+    // count people to pass result
+
     $peoplecount = $people->count();
 
+    // return view with data
 
     return view('membership', ['people' => $people, 'peoplecount' => $peoplecount, 'functiongroups' => $functiongroups, 'functionarray'=> $functionarray]);
 
