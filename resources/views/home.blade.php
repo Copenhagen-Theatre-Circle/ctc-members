927d8727c1a91bf43b9992fbbf44408e488cb958
Andrew Blackwell <andrew@balletmecanique.eu>
2018-11-26 15:59:46 +0100

    added treasure island jumbotron to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,28 +34,28 @@
                       </div>
                     </div>
 
-{{--                     <div class="jumbotron pt-3 pb-2 mb-4" style="min-height: 320px; background: no-repeat url('media/cabaret.jpg'); background-size: cover; background-position: 0% 80%; ">
+                    <div class="jumbotron pt-3 pb-2 mb-4" style="min-height: 320px; background: no-repeat url('media/treasure-island.jpg'); background-size: cover; background-position: 0% 80%; ">
                       <div class="row">
                         <div class="col-xs-1 col-sm-1 col-md-3 col-lg-4 @if ( count($codes) > 3 ) col-xl-6 @else col-xl-7 @endif " >
                         </div>
                         <div class="col mx-3 rounded" style="background-color:rgba(0, 0, 0, 0.8);">
-                          <h4 class="text-light pt-3 mb-2">Your Rebate Codes:</h4>
+                          <h4 class="text-light pt-3 mb-2">Your Treasure Island Rebate Codes:</h4>
 
                           <div @if ( count($codes) > 3 ) style="column-count: 2" @endif class="text-light mb-2">
                             @foreach ($codes as $code)
                               <p class="mb-0 @if ( count($codes) < 4 ) lead @endif">
-                              <b>{{$code->code}}</b>
+                              <b>{{strtoupper($code->code)}}</b>
                                @if ($code->person_id <> $user->person->id) &nbsp; ({{$code->first_name}}) @endif
                                @if ($code->rebate == 100) (comp) @endif
                               </p>
                             @endforeach
                           </div>
                           <div style="border-bottom: 1pt solid grey; margin-bottom: 12px;"></div>
-                          <a href="https://place2book.com/en/19541/list/EM175?event_detail=444a1742c5" target="_blank" class="btn btn-lg btn-outline-info btn-block border-white text-white mb-3">Go To Ticket Sales Page</a>
-                          <a href="ticketsales/93" class="btn btn-lg btn-outline-info btn-block border-white text-white mb-4">Check Ticket Sales Stats</a>
+                          <a href="https://place2book.com/en/list/EM175" target="_blank" class="btn btn-lg btn-outline-info btn-block border-white text-white mb-3">Go To Ticket Sales Page</a>
+                          <a href="ticketsales/95" class="btn btn-lg btn-outline-info btn-block border-white text-white mb-4">Check Ticket Sales Stats</a>
                         </div>
                       </div>
-                    </div> --}}
+                    </div>
 
                     <div class="row">
 
