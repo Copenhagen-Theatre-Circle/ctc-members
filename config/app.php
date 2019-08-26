620c6b3c808179f78aa33a9de168228cf5406b8f
Andrew Blackwell <andrew@balletmecanique.eu>
2019-08-26 22:47:01 +0200

    added barryvdh/cors

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
