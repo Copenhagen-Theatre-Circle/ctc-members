e2c5af5e85c4f9c49413f14e6c55bd371e4719f8
Andrew Blackwell <andrew@balletmecanique.eu>
2019-09-19 14:18:13 +0200

    fixed membership import bug

--- app/Http/Middleware/Member.php
+++ app/Http/Middleware/Member.php
@@ -19,20 +19,18 @@ class Member
      */
     public function handle($request, Closure $next)
     {
-      $user = \Auth::user()->id;
+        $user = \Auth::user()->id;
 
-      if ( User::find($user)->is_member )
-        {
+        if (User::find($user)->is_member) {
             return $next($request);
-        }
-        else {
+        } else {
             // import memberships
-            $active_season_id = Settings::first()->active_season_id;
-            $season = Season::where('id', $active_season_id )->first();
+            $active_season_id = Settings::first()->active_sales_season_id;
+            $season = Season::where('id', $active_season_id)->first();
             $importer = new MembershipImporter($active_season_id);
             $import = $importer->importData();
             // check again if user is member, if not: go to nomember home page
-            if ( User::find($user)->is_member){
+            if (User::find($user)->is_member) {
                 return $next($request);
             } else {
                 return response()->view('nomember', Compact('season'));
--- app/User.php
+++ app/User.php
@@ -7,56 +7,58 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    public function person()
-    {
-        return $this->hasOne('App\Person', 'mail', 'email');
-    }
+  public function person()
+  {
+    return $this->hasOne('App\Person', 'mail', 'email');
+  }
 
-    public function getIsMemberAttribute(){
-
+  public function getIsMemberAttribute()
+  {
+    if ($this->person) {
       $paid_member = $this->person->membership_this_season->count() > 0;
       $life_member = $this->person->is_life_member == 1;
       $member = $paid_member || $life_member;
-      return $member ;
-
+      return $member;
     }
+  }
 
-    public function canSeeAllPeople(){
-      //member check only happens if person is set up in database, otherwise false
-      if ($this->person['id']){
-        return $this->person['can_see_all_people'];
-      } else {
-        return false;
-      }
+  public function canSeeAllPeople()
+  {
+    //member check only happens if person is set up in database, otherwise false
+    if ($this->person['id']) {
+      return $this->person['can_see_all_people'];
+    } else {
+      return false;
     }
+  }
 
-    public function membershipid(){
-      return $this->person->membershipid();
-    }
+  public function membershipid()
+  {
+    return $this->person->membershipid();
+  }
 
-    public function uniqid(){
-      return $this->person->uniqid;
-    }
-
-
+  public function uniqid()
+  {
+    return $this->person->uniqid;
+  }
 }
