528a4185476d8bce1eddcf085806351fa5b80bc2
Andrew Blackwell <andrew@balletmecanique.eu>
2020-05-09 10:53:35 +0200

    added questionnaire link to member controller

--- app/Http/Controllers/MemberMessageController.php
+++ app/Http/Controllers/MemberMessageController.php
@@ -49,7 +49,7 @@ class MemberMessageController extends Controller
     {
         // return $_POST;
 
-        $people = Person::select('first_name','last_name','mail')
+        $people = Person::select('first_name','last_name','mail','uniqid')
                   ->whereHas('memberships', function ($q) {
                     $q->where('season_id', Settings::find(1)->active_season_id);
                   })->get();
@@ -69,6 +69,8 @@ class MemberMessageController extends Controller
           $subject = $request->subject;
           $body = $request->body;
           $body = str_replace('<<FIRST_NAME>>', $to_person->first_name, $body);
+          $link = 'https://ctc-members.dk/questionnaire/index.php?p=' . $to_person->uniqid;
+          $body = str_replace('<<QUESTIONNAIRE_LINK>>', $link, $body);
           $attributes = ['fromName' => $name_from, 'replyTo' => $mail_from, 'subject' => $subject, 'body' => $body, 'bcc' => $bcc];
           Mail::to($mail_to)->send(new ContactMessage($attributes));
         }
