720f23066ea0b4a4bed70909f56af35f02d99d31
Andrew Blackwell <andrew@balletmecanique.eu>
2021-09-27 19:53:21 +0200

    typo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,14 +36,14 @@
     </div> --}}
 
                         <div class="jumbotron pt-3 pb-2 mb-4" style="min-height: 320px;
-                                                                background-color: grey;
-                                                                background: no-repeat url('media/chekhov_dreamers.jpg');
-                                                                background-size: cover; background-position: 0% 80%; ">
+                                                                    background-color: grey;
+                                                                    background: no-repeat url('media/chekhov_dreamers.jpg');
+                                                                    background-size: cover; background-position: 0% 80%; ">
                             <div class="row">
                                 <div class="col-xs-1 col-sm-1 col-md-3 col-lg-4 @if (count($codes) > 3) col-xl-6 @else col-xl-7 @endif ">
                                 </div>
                                 <div class="col mx-3 rounded" style="background-color:rgba(0, 0, 0, 0.8);">
-                                    <h4 class="text-light pt-3 mb-2">Your Rebate Codes for 'Chekov's Dreamers':</h4>
+                                    <h4 class="text-light pt-3 mb-2">Your Rebate Codes for 'Chekhov's Dreamers':</h4>
 
                                     <div @if (count($codes) > 3) style="column-count: 2" @endif class="text-light mb-2">
                                         @foreach ($codes as $code)
